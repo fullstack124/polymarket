6032b59564926eca90d961f336443999e2baf1cf
hilalahmad32 <programmerhero6@gmail.com>
2023-02-23 22:58:09 +0500

    files

---
+++ _next/static/oQtLxc3QQgU3i59vJ0Zoi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcollection\u002F[slug]","\u002Fevent\u002F[...slug]","\u002Fmadfinale","\u002Fmarkets\u002F[[...slug]]","\u002Fportfolio"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
